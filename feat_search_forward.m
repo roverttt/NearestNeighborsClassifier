61ab871df4c621403435a2b8cf55ceacfd8a6350
roverttt <tcapp001@ucr.edu>
2021-03-18 04:29:55 -0700

    Update feat_search_forward.m

--- feat_search_forward.m
+++ feat_search_forward.m
@@ -2,22 +2,24 @@ function feat_search_forward(data)
  
 current_set_of_features = []; % Initialize an empty set
 best_accuracy_of_any_set = 0;
+accuracy_of_level_before_this = 0;
 best_set_of_features = [];
 for i = 1 : size(data,2)-1 
-    disp(['On the ',num2str(i),'th level of the search tree'])
+    %disp(['On the ',num2str(i),'th level of the search tree'])
     feature_to_add_at_this_level = 0;
     best_so_far_accuracy    = 0;    
     
      for k = 1 : size(data,2)-1 
        if isempty(intersect(current_set_of_features,k)) % Only consider adding, if not already added.
-        disp(['--Considering adding the ', num2str(k),' feature'])
+        %disp(['--Considering adding the ', num2str(k),' feature'])
         
         % Converting a list to a printable string
         % https://www.mathworks.com/matlabcentral/answers/341189-how-to-automatically-sprintf-an-array-elements
         
-        accuracy = leave_one_out_cross_validation(data,current_set_of_features,k);
-        stringToPrint1 = sprintf('%d ',[current_set_of_features, k]);
-        stringToPrint2 = sprintf('accuracy of set: %s = %d', stringToPrint1, accuracy);
+        accuracy = 100*leave_one_out_cross_validation(data,current_set_of_features,k);
+        stringToPrint1 = sprintf('%d,',[current_set_of_features, k]);
+        stringToPrint1 = stringToPrint1(1:end-1);
+        stringToPrint2 = sprintf('Using feature(s) {%s} accuracy is %s%%', stringToPrint1, num2str(accuracy,3));
         disp(stringToPrint2);
         if accuracy > best_so_far_accuracy 
             best_so_far_accuracy = accuracy;
@@ -27,8 +29,16 @@ for i = 1 : size(data,2)-1
      end
     
     current_set_of_features(i) =  feature_to_add_at_this_level;
-    disp(['On level ', num2str(i),' i added feature ', num2str(feature_to_add_at_this_level), ' to current set'])
-    disp(['Accuracy on this level = ', num2str(best_so_far_accuracy)]);
+    stringToPrint3 = sprintf('%d,',[current_set_of_features]);
+    stringToPrint3 = stringToPrint3(1:end-1);
+    stringToPrint4 = sprintf('Feature set {%s} was best, accuracy is %s%%', stringToPrint3, num2str(best_so_far_accuracy,3));
+    disp(stringToPrint4);
+    if accuracy_of_level_before_this > best_so_far_accuracy
+        disp(['(Warning, Accuracy has decreased! Continuing search in case of local maxima)']);
+    end
+    accuracy_of_level_before_this = best_so_far_accuracy;
+    %disp(['On level ', num2str(i),' i added feature ', num2str(feature_to_add_at_this_level), ' to current set'])
+    %disp(['Accuracy on this level = ', num2str(best_so_far_accuracy)]);
     
     if best_so_far_accuracy > best_accuracy_of_any_set 
         best_accuracy_of_any_set = best_so_far_accuracy;
@@ -40,8 +50,9 @@ end
 % How to format a string in matlab: https://www.mathworks.com/help/matlab/ref/disp.html
 
 stringToPrint = sprintf('%d ', best_set_of_features);
-stringToPrint = sprintf('best accuracy found with set: %s with an accuracy of %d', ...
-    stringToPrint, best_accuracy_of_any_set);
+
+stringToPrint = sprintf('Finished search!! The best feature subset is %s, which has an accuracy of %s%%', ...
+    stringToPrint, num2str(best_accuracy_of_any_set,3));
 disp(stringToPrint);
 
 end
